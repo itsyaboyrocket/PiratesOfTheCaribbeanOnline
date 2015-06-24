3ed27dc4388a293c0cd020ef0bf32c12be91e76b
Wilee999 <Infinitywilee@rocketmail.com>
2015-06-24 00:03:49 -0700

    tools: Updated the gens to have args.

--- tools/ud-gen.bat
+++ tools/ud-gen.bat
@@ -1,3 +1,8 @@
 @echo off
-python ud-gen.py
+
+rem Args: If you want the entire directory to have UDs, then add --directory. Otherwise, add the name of the distributed class (without the UD included at the end).
+rem For example, for directory, you could do: python ud-gen.py --directory
+rem Otherwise, you could do: python ud-gen.py DistributedIsland
+
+python ud-gen.py --directory
 pause
