7319392490730dce7275b0ea9d6e0d37e8f4e774
Wilee999 <Infinitywilee@rocketmail.com>
2015-06-23 23:50:41 -0700

    ud-gen: Added a ud-gen.

---
+++ tools/ud-gen.bat
@@ -0,0 +1,3 @@
+@echo off
+python ud-gen.py
+pause
---
+++ tools/ud-gen.py
@@ -0,0 +1,18 @@
+import sys, os
+
+UD = """from direct.distributed import DistributedObjectUD\n\nclass {0}(DistributedObjectUD.DistributedObjectUD):\n\n\tdef ___init___(self, air):\n\t\tDistributedObjectUD.DistributedObjectUD.__init__(self, air)\n\n\tdef announceGenerate(self):\n\t\tDistributedObjectUD.DistributedObjectUD.announceGenerate(self)\n\n\tdef generate(self):\n\t\tDistributedObjectUD.DistributedObjectUD.generate(self)\n\n\tdef delete(self):\n\t\tDistributedObjectUD.DistributedObjectUD.delete(self)\n\n\tdef disable(self):\n\t\tDistributedObjectUD.DistributedObjectUD.disable(self)\n""".expandtabs(4)
+
+dir = os.path.dirname(os.path.abspath(__file__))
+
+for distributed in os.listdir(dir):
+    print 'file: ' + distributed
+
+    if distributed.startswith("Distributed"):
+        print 'Creating UD...'
+
+        distributed = os.path.splitext(distributed)[0]
+
+        with open(distributed + "UD.py", "w") as f:
+            f.write(UD.format(distributed + "UD"))
+    else:
+        print 'Not Distributed!'
