9ef32a0ac3283fd445e9654d5ad3a58197e2c404
nkrypt <bryanmuschter@hotmail.com>
2015-06-24 17:45:24 -0400

    Add stubs from OTP

---
+++ otp/uberdog/DistributedChatManagerAI.py
@@ -0,0 +1,94 @@
+from direct.distributed.DistributedObjectAI import DistributedObjectAI
+
+class DistributedChatManagerAI(DistributedObjectAI):
+    def online(todo0):
+        #online();  // field 159
+        return
+
+    def adminChat(todo0, todo1):
+        #adminChat(uint32, string);  // field 160
+        return
+
+    def setAvatarLocation(todo0, todo1, todo2):
+        #setAvatarLocation(uint32, uint32, uint32);  // field 161
+        return
+
+    def setAvatarCrew(todo0, todo1):
+        #setAvatarCrew(uint32, uint32);  // field 162
+        return
+
+    def setAvatarGuild(todo0, todo1):
+        #setAvatarGuild(uint32, uint32);  // field 163
+        return
+
+    def chatParentId(todo0):
+        #chatParentId(uint32) airecv clsend;  // field 164
+        return
+
+    def chatZoneId(todo0):
+        #chatZoneId(uint32) airecv clsend;  // field 165
+        return
+
+    def chatFace(todo0):
+        #chatFace(uint32) airecv clsend;  // field 166
+        return
+
+    def chatEmote(todo0):
+        #chatEmote(uint16) airecv clsend;  // field 167
+        return
+
+    def chatEmoteTarget(todo0):
+        #chatEmoteTarget(uint32) airecv clsend;  // field 168
+        return
+
+    def chatIndex(todo0):
+        #chatIndex(uint16) airecv clsend;  // field 169
+        return
+
+    def chatString(todo0):
+        #chatString(string(0-256)) airecv clsend;  // field 170
+        return
+
+    def speedChatTo(todo0):
+        #speedChatTo(uint16) airecv clsend;  // field 175
+        return
+
+    def speedChatFrom(todo0, todo1):
+        #speedChatFrom(uint32, uint16);  // field 176
+        return
+
+    def speedChatCustomTo(todo0):
+        #speedChatCustomTo(uint16) airecv clsend;  // field 177
+        return
+
+    def speedChatCustomFrom(todo0, todo1):
+        #speedChatCustomFrom(uint32, uint16);  // field 178
+        return
+
+    def whisperSCTo(todo0, todo1):
+        #whisperSCTo(uint32, uint16) airecv clsend;  // field 179
+        return
+
+    def whisperSCFrom(todo0, todo1):
+        #whisperSCFrom(uint32, uint16);  // field 180
+        return
+
+    def whisperSCCustomTo(todo0, todo1):
+        #whisperSCCustomTo(uint32, uint16) airecv clsend;  // field 181
+        return
+
+    def whisperSCCustomFrom(todo0, todo1):
+        #whisperSCCustomFrom(uint32, uint16);  // field 182
+        return
+
+    def whisperSCEmoteTo(todo0, todo1):
+        #whisperSCEmoteTo(uint32, uint16) airecv clsend;  // field 183
+        return
+
+    def whisperSCEmoteFrom(todo0, todo1):
+        #whisperSCEmoteFrom(uint32, uint16);  // field 184
+        return
+
+    def whisperIgnored(todo0):
+        #whisperIgnored(uint32);  // field 185
+        return
