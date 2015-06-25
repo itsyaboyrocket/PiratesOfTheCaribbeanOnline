cd9743ec57f672b4092d0a592effb22f1f3ccfa2
nkrypt <bryanmuschter@hotmail.com>
2015-06-24 20:24:06 -0400

    forgot to push one stub

---
+++ pirates/world/DistributedGAInteriorAI.py
@@ -0,0 +1,9 @@
+
+class DistributedGAInteriorAI():
+    def setParentingRules(todo0, todo1):
+        #setParentingRules(string, string) required broadcast ram;  // field 1476
+        return
+
+    def setConnectorId(todo0):
+        #setConnectorId(uint32) required broadcast ram;  // field 1477
+        return
